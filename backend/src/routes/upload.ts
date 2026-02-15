6880fc1a2fdaa273dba7083c13e071c177a59382
Warrain <Arain1@rambler.ru>
2026-02-15 23:41:37 +0000

    Test 1.7

--- backend/src/routes/upload.ts
+++ backend/src/routes/upload.ts
@@ -1,8 +1,18 @@
 import { Router } from 'express'
 import { uploadFile } from '../controllers/upload'
 import fileMiddleware from '../middlewares/file'
+import auth, { roleGuardMiddleware } from '../middlewares/auth'
+import { Role } from '../models/user'
 
 const uploadRouter = Router()
-uploadRouter.post('/', fileMiddleware.single('file'), uploadFile)
+
+// Только админы могут загружать файлы
+uploadRouter.post(
+    '/',
+    auth,
+    roleGuardMiddleware(Role.Admin),
+    fileMiddleware.single('file'),
+    uploadFile
+)
 
 export default uploadRouter
